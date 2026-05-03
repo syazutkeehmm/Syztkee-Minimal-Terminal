e11cb8d4b8084f9a312992508c38c1106574ef13
Syazutkee <syazutkeehmm@gmail.com>
2026-05-03 20:42:48 +0800

    Add installation script for terminal starter kit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing Minimal Terminal Starter Kit..."
+
+cp .bashrc ~/.bashrc
+cp .aliases ~/.aliases
+cp .exports ~/.exports
+cp .functions ~/.functions
+
+echo "Done! Restart your terminal."
